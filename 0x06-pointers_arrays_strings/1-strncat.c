9442263b944a2848e98d5607efd6f174e2a4f03d
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-16 21:40:36 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,50 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * _strncat - concatenates two strings a defined amount
+ *
+ * @n: amount of characters/bytes from src to copy
+ *
+ * @dest: string to have content added from src
+ *
+ * @src: string to be read
+ *
+ * Return: pointer to dest after conacatenation
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	char *d;
+	char *s;
 
-	while (*(dest + count) != '\0')
+	d = dest;
+	s = src;
+
+/* increment dest address to null byte */
+	while (*dest)
 	{
-		count++;
+		dest++;
 	}
 
-	while (count2 < n)
+/*
+ * increment through src addresses n times, or until null,
+ * assigning content to end of dest
+ */
+	while (*src && (src - s) < n)
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		*dest = *src;
+		src++;
+		dest++;
 	}
+
+/* append null byte to concatenated string manually */
+	dest++;
+	*dest = '\0';
+
+/* reset pointers for dest and src to their original starting values */
+	dest = d;
+	src = s;
+
 	return (dest);
 }
