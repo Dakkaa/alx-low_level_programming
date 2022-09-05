bdd97e3dc0a417d7bea19245f906da620b28d7a2
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-09-05 14:49:06 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so
+wget -P /tmp/ https://raw.github.com/Dakkaa/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so
 export LD_PRELOAD =/tmp/liball.so
